b6c257c6709e7776bf61ed2636f182fa007f99f7
apfelkraepfla <aren.schleicher@web.de>
2023-12-11 15:05:12 +0100

    feat: :construction: solve kata for reversing/rotating chunks in a string

--- reverse_or_rotate/reverse_or_rotate.go
+++ reverse_or_rotate/reverse_or_rotate.go
@@ -1,5 +1,62 @@
 package kata
 
-func Revrot(s string, n int) string {
-	return "234561876549"
+import (
+	"strings"
+)
+
+func chunkString(input string, size int) []string {
+	endOfString := len(input)
+	var chunks []string
+	for i := 0; i < endOfString; i += size {
+		endPosition := i + size
+		if endPosition > endOfString {
+			endPosition = endOfString
+		}
+		chunks = append(chunks, input[i:endPosition])
+	}
+	return chunks
 }
+
+func rotateStringToLeft(input string) string {
+	return input[1:] + input[:1]
+}
+
+func reverseString(input string) string {
+	runes := []rune(input)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func sumOfCubes(input string) int {
+	var sumOfCubes int
+	for _, i := range input {
+		// get int representation of digit from an ASCII value
+		number := int(i - '0')
+		sumOfCubes += number * number * number
+	}
+	return sumOfCubes
+}
+
+func isSumOfCubesOfDigitsDivisibleByTwo(input string) bool {
+	return sumOfCubes(input)%2 == 0
+}
+
+func Revrot(s string, n int) string {
+	var revisedChunks []string
+	chunks := chunkString(s, n)
+
+	for _, chunk := range chunks {
+		if len(chunk) == n {
+			if !isSumOfCubesOfDigitsDivisibleByTwo(chunk) {
+				revisedChunks = append(revisedChunks, rotateStringToLeft(chunk))
+			} else {
+				revisedChunks = append(revisedChunks, reverseString(chunk))
+			}
+		}
+	}
+	return strings.Join(revisedChunks, "")
+}
+
+// revrot("123456987654", 6) --> "234561876549"
--- reverse_or_rotate/reverse_or_rotate_test.go
+++ reverse_or_rotate/reverse_or_rotate_test.go
@@ -2,13 +2,61 @@ package kata
 
 import "testing"
 
-func TestReverseOrRotate(t *testing.T) {
-	t.Run("first example", func(t *testing.T) {
-		got := Revrot("123456987654", 6)
-		want := "234561876549"
-
-		if got != want {
-			t.Errorf("expected %s, but got %s", want, got)
-		}
+func TestSumOfCubes(t *testing.T) {
+	t.Run("only '1's", func(t *testing.T) {
+		assertEqualInt(t, 4, sumOfCubes("1111"))
 	})
+	t.Run("different numbers", func(t *testing.T) {
+		assertEqualInt(t, 352, sumOfCubes("172"))
+	})
+}
+
+func TestReverseStrings(t *testing.T) {
+	t.Run("string with even length", func(t *testing.T) {
+		assertEqualStrings(t, "654321", reverseString("123456"))
+	})
+	t.Run("string with odd length", func(t *testing.T) {
+		assertEqualStrings(t, "42", reverseString("24"))
+	})
+	t.Run("only one digit", func(t *testing.T) {
+		assertEqualStrings(t, "7", reverseString("7"))
+	})
+}
+
+func TestRotateStringToLeft(t *testing.T) {
+	t.Run("string with several digits", func(t *testing.T) {
+		assertEqualStrings(t, "234561", rotateStringToLeft("123456"))
+	})
+	t.Run("string with one digit", func(t *testing.T) {
+		assertEqualStrings(t, "2", rotateStringToLeft("2"))
+	})
+}
+
+func TestReverseOrRotate(t *testing.T) {
+	t.Run("string with rotated chunks of length '6'", func(t *testing.T) {
+		assertEqualStrings(t, "234561876549", Revrot("123456987654", 6))
+	})
+	t.Run("string with reversed chunks of length '6'", func(t *testing.T) {
+		assertEqualStrings(t, "234561356789", Revrot("123456987653", 6))
+	})
+	t.Run("string with chunks of length '4'", func(t *testing.T) {
+		assertEqualStrings(t, "44668753", Revrot("66443875", 4))
+	})
+	t.Run("ignore last chunk if smaller than given chunks size", func(t *testing.T) {
+		assertEqualStrings(t, "67834466", Revrot("664438769", 8))
+	})
+}
+
+func assertEqualInt(t *testing.T, want, got int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("expected %d, but got %d", want, got)
+	}
+}
+
+func assertEqualStrings(t *testing.T, want, got string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("expected %s, but got %s", want, got)
+	}
 }
