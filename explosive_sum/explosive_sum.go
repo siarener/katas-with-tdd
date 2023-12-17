c15fda009f024c2b4370c95e8c02a0c012d3b2e6
apfelkraepfla <aren.schleicher@web.de>
2023-12-17 15:10:16 +0100

    refactor: :recycle: reduce to only handling one row instead of whole table

--- explosive_sum/explosive_sum.go
+++ explosive_sum/explosive_sum.go
@@ -36,29 +36,100 @@ See here for more examples. */
 package kata
 
 func ExpSum(u uint64) uint64 {
-	if u == 0 {
-		return uint64(1)
-	}
 
-	dp := make([][]uint64, u+1)
-	for i := range dp {
-		dp[i] = make([]uint64, u+1)
-	}
+	sumOptions := make([]uint64, u+1)
+	sumOptions[0] = 1
 
-	for i := 0; i <= int(u); i++ {
-		dp[i][1] = 1
-		dp[0][i] = 1
+	/* My former solution is replaced by the [solution from Noob's blog](https://yurukute.github.io/Blog/en/post/explosivesum/),
+	    as it couldn't be beat in it's simplicity. I rewrote the
+		explanation in the following with more in-between steps,
+		to grasp better what happens in the program.
 
-	}
+		For the calculation for the options we build a table, until
+		we get to the row and cell where our result is located. As
+		we only need the last row to compute the following row, we
+		don't need to save the rows before the last row.
 
-	for i := 1; i <= int(u); i++ {
-		dp[i][1] = 1
-		for j := 2; j <= int(u); j++ {
-			dp[i][j] = dp[i][j-1]
-			if i >= j {
-				dp[i][j] += dp[i-j][j]
-			}
+		Example: Finding the options to find the partitions of '4'
+
+			Starting row:
+						  | 0  1  2  3  4
+						------------------
+						0 | 1  0  0  0  0
+
+			1. row calculation:
+					The values from the starting row are used to calculate the new row.
+
+					'sumOptions[cellIdx] += sumOptions[cellIdx-rowIdx]'
+					->
+					row[1] = row[0] + row[1]
+							where
+							-> row[0] holds the value from the starting row, i.e. '1'
+							-> row[1] holds the value from the starting row, i.e. '0'
+						-> cell row[1] of the currently calculated row now holds the value '1'
+					...
+
+						  | 0  1  2  3  4
+						------------------
+						1 | /  1  1  1  1
+
+
+			2. row calculation:
+
+					'sumOptions[cellIdx] += sumOptions[cellIdx-rowIdx]'
+					->
+					row[2] = row[2] + row[2-2]
+							where
+							-> row[2] holds the value from the former row, i.e. '1'
+							-> row[0] holds the value from the starting row, i.e. '1'
+						-> cell row[2] of the currently calculated row now holds the value '2'
+					...
+					row[4] = row[4] + row[4-2]
+							where
+							-> row[4] holds the value from the former row, i.e. '1'
+							-> row[2] holds the value from the current row, i.e. '2'
+						-> cell row[4] of the currently calculated row now holds the value '3'
+					...
+						  | 0  1  2  3  4
+						------------------
+						2 | /  /  2  2  3
+
+			3. row calculation:
+						  | 0  1  2  3  4
+						------------------
+						3 | /  /  /  3  4
+
+			4. row calculation:
+						  | 0  1  2  3  4
+						------------------
+						4 | /  /  /  /  5
+
+
+			If we would add what we calculate in each loop, we have:
+						  | 0  1  2  3  4
+						------------------
+						0 | 1  0  0  0  0
+						1 | /  1  1  1  1
+						2 | /  /  2  2  3
+						3 | /  /  /  3  4
+						4 | /  /  /  /  5
+
+			We can skip calculating one more cell value each loop while progressing each loop,
+			as we use the value still stored from the last row for the calculation.
+			The '/' shows that the respective value that was not calculated. As we
+			are always using the same row, this means that this cell still holds the last
+			value assigned to it, i.e. in the last loop, the value in row[0] is still from
+			the starting row, the value in row[1] is from the 1st loop, the value in row[2]
+			from the 2nd loop, etc., so our final row looks like this:
+						  | 0  1  2  3  4
+						------------------
+						4 | 1  1  2  3  5
+	*/
+	for rowIdx := uint64(1); rowIdx <= u; rowIdx++ {
+		for cellIdx := rowIdx; cellIdx <= u; cellIdx++ {
+			sumOptions[cellIdx] += sumOptions[cellIdx-rowIdx]
 		}
 	}
-	return uint64(dp[u][u])
+
+	return sumOptions[u]
 }
